395e7e6a6b205930fafa8f5231a66431e6f23188
I Putu Agestya Pramana <agespramana9@gmail.com>
2024-04-30 13:57:04 +0700

    Feat: add new config at header axios

--- app/lib/services.ts
+++ app/lib/services.ts
@@ -2,10 +2,11 @@ import ax from "axios";
 
 export const axios = ax.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  // headers: {
-  //   "Content-Type": "application/json",
-  //   // "X-Requested-With": "XMLHttpRequest",
-  // },
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept",
+  },
   // withCredentials: true,
   // withXSRFToken: true,
 });
